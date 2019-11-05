4a4eda1378218733b863b75b1295af2ca673bfd1
shonali-ks <50069308+shonali-ks@users.noreply.github.com>
2019-11-05 23:21:05 +0530

    final code

--- project.m
+++ project.m
@@ -1,5 +1,8 @@
- %Reading image
- rgb = imread('th.jpg'); 
+tic;
+initime = cputime;
+time1   = clock;
+%Reading image
+ rgb = imread('th1.jpg'); 
                                                                            
 figure(1)
 imshow(rgb)
@@ -9,26 +12,30 @@ imshow(rgb)
 gray = rgb2gray(rgb);  
 figure(2)                                                                                              
 imshow(gray);
-kernel1 = -1 * ones(3)/9;
-kernel1(2,2) = 8/9;
+%gray=imresize(gray,size(rgb));
+
+
 
 
 %Applying gaussian filter to image for noise removal
 
 gauss=imgaussfilt(gray,1);
 
+[peaksnr,snr]= psnr(gray, gauss);
+fprintf('\n The Peak-SNR value is %0.4f\n', peaksnr);
+fprintf('\n The SNR value is %0.4f \n', snr);
 
 %Enhances the quality of the image
 %eroding the image
 
-SE = strel('diamond',10);
+SE = strel('diamond',5);
 I= imerode(gauss,SE);
 figure(3)
 imshow(I)
 
 %dialating the image
 
-se=strel('diamond',5);
+se=strel('diamond',10);
 J = imdilate(I,se);
 figure(4)
 imshow(J)
@@ -66,9 +73,9 @@ imshow(img3)
 %threshodling and converting it into binary image
 
 level = graythresh(img3);
-BW = im2bw(img3,level);
+%BW = im2bw(img3,level);
 
-%BW = im2bw(img3,level); (if matlab version is >=2016
+BW = im2bw(img3,level); %(if matlab version is >=2016
 
 figure(10)
 imshow(BW)
@@ -90,7 +97,14 @@ imcontour(img3)
 figure(14)
 C = contour(img3);
 clabel(C)
-
 %contourf for tumour
 figure (15)
 contourf(img3)
+
+
+fintime = cputime;
+elapsed = toc;
+time2   = clock;
+fprintf('TIC TOC: %g\n', elapsed);
+fprintf('CPUTIME: %g\n', fintime - initime);
+fprintf('CLOCK:   %g\n', etime(time2, time1));
